61c0b153181efc7907b5070de23f5879abc37205
Jeffrey <jrhicks@cteh.com>
2016-03-26 14:02:35 -0500

    write cart created event to eventstore

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var gesClient = require('../lib/event-store-client')();
 var repo = require('../lib/event-store-repo');
+var eventstore = require('geteventstore-promise');
 
 /* POST cart command */
 router.post('/carts/createcart', function(req, res, next) {
     var stream = repo.createCart();
-    res.write(stream);
-    res.end();
+    var events = [eventstore.eventFactory.NewEvent('cartCreated', {})];
+    gesClient.writeEvents(stream, events).then( function(events) {
+      res.write(stream);
+      res.end();
+    });
 });
 
 router.post('/carts/:id/addproduct', function(req, res, next){

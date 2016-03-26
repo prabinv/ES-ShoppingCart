22dd9ef3f6dd41ff44eefdbe4daa87ba2a3ed970
Andy Siemer <andy_siemer@volusion.com>
2016-03-26 13:09:25 -0500

    new routes

--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* POST cart command */
-router.post('/createcart', function(req, res, next) {
+router.post('/carts/createcart', function(req, res, next) {
     
+});
+
+router.post('/carts/:id/addproduct', function(req, res, next){
+
+});
+
+reouter.post('/carts/:id/removeproduct', function(req, res, next){
+
 });
 
 module.exports = router;

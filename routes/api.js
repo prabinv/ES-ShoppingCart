2b493ed835cb6a29fa437e3f8ac11c3db3c77181
Andy Siemer <andy_siemer@volusion.com>
2016-03-26 13:40:57 -0500

    added create cart end point

--- routes/api.js
+++ routes/api.js
@@ -1,16 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var repo = require('../lib/event-store-repo');
 
 /* POST cart command */
 router.post('/carts/createcart', function(req, res, next) {
-    
+    var stream = repo.createCart();
+    res.write(stream);
+    res.end();
 });
 
 router.post('/carts/:id/addproduct', function(req, res, next){
 
 });
 
-reouter.post('/carts/:id/removeproduct', function(req, res, next){
+router.post('/carts/:id/removeproduct', function(req, res, next){
 
 });
 

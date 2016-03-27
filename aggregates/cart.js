245a447e9948deb908948a5d3c11de10ce2b8910
Jeffrey <jrhicks@cteh.com>
2016-03-26 21:54:53 -0500

    I had to use the tcp client to read events.  hydrate (aka reconstitute) works now

--- aggregates/cart.js
+++ aggregates/cart.js
@@ -1,20 +1,78 @@
-var gesClient = require('../lib/event-store-client')();
+var httpClient = require('../lib/event-store-client')();
+var tcpClient = require('../lib/event-store-tcp-client')();
 var eventstore = require('geteventstore-promise');
+var uuid = require('node-uuid');
 
-var Cart = function() {
+var Cart = function(stream) {
+    this.lastEventNumber = -1;
+    if (stream) {
+      this.stream = stream;
+    } else {
+      this.stream = 'cart-' + uuid.v4();
+    }
     this.productQtys = {}; // mapping of product ids to qty in cart
     this.errors = [];
 
+    this.reconstitute = function(cb) {
+      var page = {start: 0, length: 1000};
+      this.constituteHelper(page, cb);
+    }
+
+    this.constituteHelper = function(page, cb) {
+      var self = this;
+      tcpClient.getEvents(this.stream, page.start, page.length, 'forward').then(function(events){
+        events.forEach(function(event) {
+          self.apply(event);
+        });
+        cb();
+      });
+    }
+
+    // commands
+    this.createCart = function(cb) {
+      var events = [eventstore.eventFactory.NewEvent('cartCreated', {})];
+      httpClient.writeEvents(this.stream, events).then( function() {
+        cb(events);
+      });
+    }
+
+    this.addProduct = function(payload, cb) {
+      var events = [eventstore.eventFactory.NewEvent('productAddedToCart', {
+        cartID: this.stream,
+        productID: payload.productID,
+        productQuantity: payload.productQuantity
+      })];
+      httpClient.writeEvents(this.stream, events).then( function() {
+        cb(events);
+      });
+    }
+
+    this.removeProduct = function(payload, cb) {
+      console.log('removeCart');
+      var events = [eventstore.eventFactory.NewEvent('productRemovedFromCart', {
+        cartID: this.stream,
+        productID: payload.productID,
+        productQuantity: payload.productQuantity
+      })];
+      httpClient.writeEvents(this.stream, events).then( function() {
+        cb(events);
+      });
+    }
+
+    // events
     this.apply = function(event) {
-      switch (event.type) {
+      this.lastEventNumber = event.eventNumber;
+      switch (event.eventType ) {
+        case 'cartCreated':
+          break;
         case 'productAddedToCart':
-          this.productAddedToCart(event.payload);
+          this.productAddedToCart(event.data);
           break;
         case 'productRemovedFromCart':
-          this.productRemovedFromCart(event.payload);
+          this.productRemovedFromCart(event.data);
           break;
         default:
-          this.errors.push('Unhandled Event '+event.type);
+          this.errors.push('Unhandled Event '+ event.eventType);
           break;
       }
     }
@@ -29,6 +87,7 @@ var Cart = function() {
         // if this product is not in the cart add it
         this.productQtys[productId] = productQty;
       }
+      console.log(JSON.stringify(this.productQtys, null, 2));
     }
 
     this.productRemovedFromCart = function(payload) {
--- routes/api.js
+++ routes/api.js
@@ -1,52 +1,46 @@
 var express = require('express');
 var router = express.Router();
-var gesClient = require('../lib/event-store-client')();
-var repo = require('../lib/event-store-repo');
-var eventstore = require('geteventstore-promise');
 var Cart = require('../aggregates/Cart');
 
 /* POST cart command */
 router.post('/carts/createcart', function(req, res, next) {
-    var stream = repo.createCart();
-    var events = [eventstore.eventFactory.NewEvent('cartCreated', {})];
-    gesClient.writeEvents(stream, events).then( function(events) {
-      res.write(stream);
-      res.end();
-    });
+  var cart = new Cart();
+  cart.createCart( function(events) {
+    res.write(cart.stream);
+    res.end();
+  });
 });
 
 router.post('/carts/:id/addproduct', function(req, res, next){
   var stream = req.params.id;
-  var payload = req.body;
-  var events = [eventstore.eventFactory.NewEvent('productAddedToCart', {
-    cartID: stream,
-    productID: payload.productID,
-    productQuantity: payload.productQuantity
-
-  })];
-  gesClient.writeEvents(stream, events).then( function(events) {
-      res.write(stream);
+  var cart = new Cart(stream);
+  cart.reconstitute( function() {
+    cart.addProduct(req.body, function(events) {
+      res.write(JSON.stringify(events));
       res.end();
+    });
   });
 });
 
 router.post('/carts/:id/removeproduct', function(req, res, next){
+  var stream = req.params.id;
+  var cart = new Cart(stream);
 
+  cart.reconstitute( function() {
+    cart.removeProduct(req.body, function(events) {
+      res.write(JSON.stringify(events));
+      res.end();
+    });
+  });
 });
 
 router.get('/carts/:id/products', function(req, res, next){
   var stream = req.params.id;
-  var cart = new Cart();
-  console.log("Calling gesClient.getEvents ...");
-  gesClient.getEvents(stream).then( function(events) {
-      console.log("gesClient resolved!"); // This isn't happening
+  var cart = new Cart(stream);
 
-      // I'm assuming we could just loop over events and apply
-      events.forEach(function(event) {
-        cart.apply(event);
-      });
-      res.write(JSON.stringify(cart.productQtys, null, 2));
-      res.end();
+  cart.reconstitute( function(events) {
+    res.write(JSON.stringify(cart.productQtys));
+    res.end();
   });
 });
 
